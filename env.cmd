23da5e5ae86058250dd9b6ed04aa895e8b806ba1
strangenewcoder <strangenewcoder@gmail.com>
2026-05-03 16:43:23 +0200

    file structure modification

--- env.cmd
+++
@@ -1,5 +0,0 @@
-set PROJECTDIR=f:\projects\veeamdesigner
-call %PROJECTDIR%\venv\scripts\activate.bat
-set PATH=%PATH%C:\Program Files\Python314\scripts;
-set PYTHONPATH=%PROJECTDIR%\modules
-set STYLES=%PROJECTDIR%\styles
